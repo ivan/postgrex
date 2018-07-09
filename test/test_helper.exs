22082656614f8255fa2bb8a98b19bf92e0a96799
Ivan Kozik <ivan@ludios.org>
2018-07-09 15:21:02 +0000

    Add PGFLAVOR env var for tests and don't run PostgreSQL-only SQL on CockroachDB

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -6,6 +6,13 @@ pg_version =
       {String.to_integer(major), String.to_integer(minor || "0")}
   end
 
+pg_flavor =
+  case System.get_env("PGFLAVOR") do
+    nil -> :postgresql
+    "postgresql" -> :postgresql
+    "cockroachdb" -> :cockroachdb
+  end
+
 otp_release = :otp_release |> :erlang.system_info() |> List.to_integer()
 unix_socket_dir = System.get_env("PG_SOCKET_DIR") || "/tmp"
 port = System.get_env("PGPORT") || "5432"
@@ -48,31 +55,38 @@ run_cmd = fn cmd ->
 end
 
 sql = """
-DROP ROLE IF EXISTS postgrex_cleartext_pw;
-DROP ROLE IF EXISTS postgrex_md5_pw;
-
-CREATE USER postgrex_cleartext_pw WITH PASSWORD 'postgrex_cleartext_pw';
-CREATE USER postgrex_md5_pw WITH PASSWORD 'postgrex_md5_pw';
-
 DROP TABLE IF EXISTS composite1;
 CREATE TABLE composite1 (a int, b text);
 
 DROP TABLE IF EXISTS composite2;
 CREATE TABLE composite2 (a int, b int, c int);
 
-DROP TYPE IF EXISTS enum1;
-CREATE TYPE enum1 AS ENUM ('elixir', 'erlang');
-
 CREATE TABLE uniques (a int UNIQUE);
 
 DROP TABLE IF EXISTS missing_oid;
-DROP TYPE IF EXISTS missing_enum;
-DROP TYPE IF EXISTS missing_comp;
 
 CREATE TABLE altering (a int2);
 
 CREATE TABLE calendar (a timestamp without time zone, b timestamp with time zone);
+"""
 
+sql_roles = """
+DROP ROLE IF EXISTS postgrex_cleartext_pw;
+DROP ROLE IF EXISTS postgrex_md5_pw;
+
+CREATE USER postgrex_cleartext_pw WITH PASSWORD 'postgrex_cleartext_pw';
+CREATE USER postgrex_md5_pw WITH PASSWORD 'postgrex_md5_pw';
+"""
+
+sql_types = """
+DROP TYPE IF EXISTS enum1;
+CREATE TYPE enum1 AS ENUM ('elixir', 'erlang');
+
+DROP TYPE IF EXISTS missing_enum;
+DROP TYPE IF EXISTS missing_comp;
+"""
+
+sql_domains = """
 DROP DOMAIN IF EXISTS points_domain;
 CREATE DOMAIN points_domain AS point[] CONSTRAINT is_populated CHECK (COALESCE(array_length(VALUE, 1), 0) >= 1);
 
@@ -90,7 +104,13 @@ cmds = [
   ["-c", "DROP DATABASE IF EXISTS postgrex_test_with_schemas;"],
   ["-c", "CREATE DATABASE postgrex_test TEMPLATE=template0 ENCODING='UTF8' LC_COLLATE='C.UTF-8' LC_CTYPE='C.UTF-8';"],
   ["-c", "CREATE DATABASE postgrex_test_with_schemas TEMPLATE=template0 ENCODING='UTF8' LC_COLLATE='C.UTF-8' LC_CTYPE='C.UTF-8';"],
-  ["-d", "postgrex_test", "-c", sql],
+  ["-d", "postgrex_test", "-c", sql]
+]
+
+postgresql_cmds = [
+  ["-d", "postgrex_test", "-c", sql_roles],
+  ["-d", "postgrex_test", "-c", sql_types],
+  ["-d", "postgrex_test", "-c", sql_domains],
   ["-d", "postgrex_test_with_schemas", "-c", sql_with_schemas]
 ]
 
@@ -98,15 +118,18 @@ pg_path = System.get_env("PGPATH")
 
 cmds =
   cond do
-    !pg_version || pg_version >= {9, 1} ->
-      cmds ++ [["-d", "postgrex_test_with_schemas", "-c", "CREATE EXTENSION IF NOT EXISTS hstore WITH SCHEMA test;"],
-               ["-d", "postgrex_test", "-c", "CREATE EXTENSION IF NOT EXISTS hstore;"]]
-    pg_version == {9, 0} ->
-      cmds ++ [["-d", "postgrex_test", "-f", "#{pg_path}/contrib/hstore.sql"]]
-    pg_version < {9, 0} ->
-      cmds ++ [["-d", "postgrex_test", "-c", "CREATE LANGUAGE plpgsql;"]]
-    true ->
+    pg_flavor == :cockroachdb ->
       cmds
+    !pg_version || pg_version >= {9, 1} ->
+      cmds ++ postgresql_cmds ++
+        [["-d", "postgrex_test_with_schemas", "-c", "CREATE EXTENSION IF NOT EXISTS hstore WITH SCHEMA test;"],
+         ["-d", "postgrex_test", "-c", "CREATE EXTENSION IF NOT EXISTS hstore;"]]
+    pg_version == {9, 0} ->
+      cmds ++ postgresql_cmds ++ [["-d", "postgrex_test", "-f", "#{pg_path}/contrib/hstore.sql"]]
+    pg_version < {9, 0} ->
+      cmds ++ postgresql_cmds ++ [["-d", "postgrex_test", "-c", "CREATE LANGUAGE plpgsql;"]]
+    true ->
+      cmds ++ postgresql_cmds
 end
 
 psql_env = Map.put_new(System.get_env(), "PGUSER", "postgres")
